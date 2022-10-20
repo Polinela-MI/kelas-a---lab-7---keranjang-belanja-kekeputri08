82605c02c497f0c8dadb24d5418b302b0df5729d
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-10-20 10:58:09 +0700

    1

---
+++ src/OnlineShop.java
@@ -0,0 +1,28 @@
+public class OnlineShop {
+    String namaBarang;
+    int hargaBarang;
+
+
+    OnlineShop(String namaBarang, int hargaBarang){
+        this.namaBarang=namaBarang;
+        this.hargaBarang=hargaBarang;
+    }
+
+    public void setHargaBarang(int hargaBarang) {
+        this.hargaBarang = hargaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public int getHargaBarang() {
+        return hargaBarang;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+}
+
+
